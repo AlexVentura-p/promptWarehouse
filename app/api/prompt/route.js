7e7edc9d45e98106baa6a23ecfc57a03ac8d6b64
AlexVentura-p <alex.ventura300@gmail.com>
2023-06-13 17:54:39 -0600

    Fix bug

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,10 +4,22 @@ import Prompt from "@models/prompt";
 export const GET = async (request) => {
     try {
         await connectToDB();
-        const prompts = await Prompt.find({}).populate('creator');
+        const prompts = await Prompt.find().populate({
+          path: "creator"
+        });
 
-        return new Response(JSON.stringify(prompts),
-        {status: 200})
+        const response = new Response(JSON.stringify(prompts), {
+          status: 200,
+        });
+
+        const url = new URL(request.url);
+        url.searchParams.set("t", Date.now());
+        response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.headers.set("Pragma", "no-cache");
+        response.headers.set("Expires", "0");
+        response.headers.set("Location", url.toString());
+
+        return response;
 
     } catch (error) {
         return new Response(error,
